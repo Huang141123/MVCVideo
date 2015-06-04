a2047eb2259db169726b33e3cbc09db86a20503a
19891206hwl@sina.com <19891206hwl@sina.com>
2015-06-04 14:34:45 +0800

    150604

---
+++ MVCVideo/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCVideo.IService;
+using MVCVideo.Service;
+
+namespace MVCVideo.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public HelloWorldController() : this(new HelloService())
+        {
+            
+        }
+
+        private IHelloService _helloService;
+        public HelloWorldController(HelloService helloService)
+        {
+            _helloService = helloService;
+        }
+
+        public String Welcome()
+        {
+            return _helloService.GetHelloWorld();
+        }
+    }
+}
---
+++ MVCVideo/Service/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCVideo.IService;
+
+namespace MVCVideo.Service
+{
+    public class HelloService:IHelloService
+    {
+
+        public string GetHelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
